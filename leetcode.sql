ac404168d50321aa0a1b8150609796d40d420bb7
LePhongCongThanh <125122257+LePhongCongThanh@users.noreply.github.com>
2023-07-10 18:59:15 +0700

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -6,3 +6,14 @@ from Queries
 SELECT query_name, round(avg(c),2) as quality, round(sum(d)*100/count(d),2) as poor_query_percentage
 FROM CTE
 group by query_name
+
+https://leetcode.com/problems/monthly-transactions-i/submissions/990884899/
+# Write your MySQL query statement below 
+WITH CTE as ( SELECT id,country, state, amount, DATE_FORMAT(trans_date,'%Y-%m') as month
+FROM TRANSACTIONS)
+SELECT month, country, count(Amount) as trans_count, 
+sum(if(state = "approved",1,0)) as approved_count, 
+sum(amount) as trans_total_amount,
+sum(if(state = "approved", amount, 0)) as approved_total_amount
+FROM CTE
+GROUP BY month, country
